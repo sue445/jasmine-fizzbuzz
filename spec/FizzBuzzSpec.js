ea403a925cc1d80980e25e484ed911f2e5f87181
sue445 <sue445@sue445.net>
2013-07-15 13:54:33 +0900

    add red test

---
+++ spec/FizzBuzzSpec.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: sue445
+ * Date: 2013/07/15
+ * Time: 13:44
+ * To change this template use File | Settings | File Templates.
+ */
+describe("FizzBuzz", function(){
+    describe("#show", function(){
+        it("3の時はFizzが返ってくる", function(){
+            var actual = fizzBuzz.show(3);
+            expect(actual).toEqual("Fizz");
+        });
+    })
+});
---
+++ src/FizzBuzz.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: sue445
+ * Date: 2013/07/15
+ * Time: 13:44
+ * To change this template use File | Settings | File Templates.
+ */
+var fizzBuzz = {
+    show : function(str){
+
+    }
+}
